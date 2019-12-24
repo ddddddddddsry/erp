22dfbd5dfebc3cee6c9e31dfa89ef3e8878f72e1
wyy <287571899@qq.com>
2019-12-24 16:54:26 +0800

    侧边栏

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 const Login = () =>
   import(/* webpackChunkName:'Login' */ '../components/login/Login.vue') // 登陆页面
-const Home = () =>
-  import(/* webpackChunkName:'Login' */ '../components/Home.vue') // 登陆页面
-const Discount = () => import('../components/discount/discount.vue')// 数据分析页面
-const AcceptanceBank = () => import('../components/discount/acceptanceBank.vue')// 序列对比页面
-const FinanceCompany = () => import('../components/discount/financeCompany.vue')// 测试分析模式
-const Member = () => import('../components/discount/member.vue')// 测试分析模式
+const Home = () => import('../components/Home.vue') // 首页页面
+const Welcome = () => import('../components/Welcome.vue')// 欢迎页面
+const Discount = () => import('../components/discount/discount.vue')// 贴现管理页面
+const AcceptanceBank = () => import('../components/discount/acceptanceBank.vue')// 承兑银行页面
+const FinanceCompany = () => import('../components/discount/financeCompany.vue')// 财务公司
+const Member = () => import('../components/discount/member.vue')// 会员
 
 Vue.use(VueRouter)
 
@@ -16,9 +16,9 @@ const routes = [
   { path: '/Login', component: Login }, // 登录
   { path: '/Home',
     component: Home,
-    // redirect: '/Welcome',
+    redirect: '/Welcome',
     children: [
-      // { path: '/Welcome', component: Welcome },
+      { path: '/Welcome', component: Welcome },
       { path: '/discount', component: Discount },
       { path: '/acceptanceBank', component: AcceptanceBank },
       { path: '/financeCompany', component: FinanceCompany },
